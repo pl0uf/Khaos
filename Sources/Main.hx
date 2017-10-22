d7c8e4c8ce698f826119807493a394331d22e754
pl0uf <32980426+pl0uf@users.noreply.github.com>
2017-10-22 13:47:22 +0200

    Change html5 display size

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -1,10 +1,28 @@
 package;
 
 import kha.System;
+#if js
+import js.html.CanvasElement;
+import js.Browser.document;
+#end
 
 class Main {
+  public static inline var WIDTH:Int = 1600;
+  public static inline var HEIGHT:Int = 900;
+
 	public static function main() {
-		System.init({title: "Khaos", width: 1600, height: 900}, function () {
+    #if js
+    document.documentElement.style.padding = "0";
+    document.documentElement.style.margin = "0";
+    document.body.style.padding = "0";
+    document.body.style.margin = "0";
+    var canvas = cast(document.getElementById("khanvas"), CanvasElement);
+    canvas.style.display = "block";
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    #end
+
+		System.init({title: "Khaos", width: WIDTH, height: HEIGHT}, function () {
 			new Game();
 		});
 	}
